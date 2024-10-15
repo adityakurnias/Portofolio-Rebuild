345aaee57016ddf1f34586465ba4fc6cf482b9d6
adityakurnias <adit34072@gmail.com>
2024-10-15 23:22:24 +0700

    Adding folders that will be useful later

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
+import { Title } from "#build/components";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        {
+          "name": "viewport",
+          "content": "width=device-width, initial-scale=1"
+        },
+        {
+          "charset": "utf-8"
+        },
+      ]
+    }
+  },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
   modules: ['@nuxtjs/tailwindcss', '@nuxt/image', '@nuxt/icon']
 })
