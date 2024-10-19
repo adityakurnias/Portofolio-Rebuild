d8ec93291e73ade0e139df532f7bedb9bf1210d9
adityakurnias <adit34072@gmail.com>
2024-10-19 20:17:06 +0700

    Add Lenis

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav id="nav"
     class=" w-screen h-fit py-5 px-16 fixed bg-transparent flex items-center justify-between pointer-events-auto">
     <span class=" text-3xl font-mono">Aditya.</span>
-    <NuxtImg
+    <img
       class=" w-12 justify-self-end transition-transform duration-100 ease-in-out hover:rotate-[360deg] hover:scale-110"
       src="/img/Icon.png" />
   </nav>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { Title } from "#build/components";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
